68771fe11e3070e310caf16f5b837fa5c9df33b5
od-k-ap-12 <predictormagnolia@gmail.com>
2023-10-20 23:23:43 +0300

    resource dictionaries added

--- 1020 localization/MainWindow.xaml.cs
+++ 1020 localization/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace _1020_localization
     /// </summary>
     public partial class MainWindow : Window
     {
+        string Language {  get; set; }  
         public MainWindow()
         {
             InitializeComponent();
+            Language = "EN";
+        }
+        private void btnChangeLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            if (Language == "EN")
+            {
+                ResourceDictionary newDictionary = new ResourceDictionary();
+                newDictionary.Source = new Uri("Resources/DictionaryUA.xaml", UriKind.Relative);
+                Application.Current.Resources.MergedDictionaries[0] = newDictionary;
+                Language = "UA";
+            }
+            else if (Language == "UA")
+            {
+                ResourceDictionary newDictionary = new ResourceDictionary();
+                newDictionary.Source = new Uri("Resources/DictionaryEN.xaml", UriKind.Relative);
+                Application.Current.Resources.MergedDictionaries[0] = newDictionary;
+                Language = "EN";
+            }
         }
     }
 }
